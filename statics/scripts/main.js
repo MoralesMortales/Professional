489d6a4f1f6466a690a6f02b2b00bc7f6a2b4c79
CarlosLup <moralesyaguilera.com>
2024-05-03 22:24:28 -0400

    navbar

--- statics/scripts/main.js
+++ statics/scripts/main.js
@@ -1,3 +1,27 @@
 ScrollReveal().reveal('.section', {
     duration: 1000, // Animation duration in milliseconds
 });
+
+document.getElementById('switch');
+
+  
+  document.getElementById("switch").addEventListener("click", function(event) {
+    var nav = document.getElementById("nav-bar");
+    var bottom = document.getElementById("switch");
+    nav.classList.toggle("hidden");
+    bottom.classList.toggle("hidden");
+    event.stopPropagation(); // Evita que el clic en el botón propague al div
+  });
+  
+  // Event listener para cerrar el div cuando se haga clic en otra parte de la pantalla
+  document.addEventListener("click", function(event) {
+    let nav = document.getElementById("nav-bar");
+    var bottom = document.getElementById("switch");
+     
+    if (!nav.contains(event.target)) {
+      nav.classList.add("hidden");
+      bottom.classList.remove("hidden");
+
+    }
+  });
+  
