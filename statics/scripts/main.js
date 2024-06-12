cda455cd06fb3b0dfa8c975ecdc1e7bddbb8966b
CarlosLup <moralesyaguilera.com>
2024-06-12 13:46:06 -0400

    slide

--- statics/scripts/main.js
+++ statics/scripts/main.js
@@ -24,4 +24,35 @@ document.getElementById('switch');
 
     }
   });
-  
+
+  // scripts.js
+  document.addEventListener('DOMContentLoaded', () => {
+    const carousel = document.querySelector('#Programming-languages-boxes');
+    let currentIndex = 0;
+    let interval;
+
+    const updateCarousel = (index) => {
+        const totalSlides = carousel.children.length;
+        const slideWidth = carousel.children[0].offsetWidth + parseFloat(getComputedStyle(carousel.children[0]).marginRight) * 2;
+        const offset = -index * slideWidth;
+        carousel.style.transform = `translateX(${offset}px)`;
+    };
+
+    const nextSlide = () => {
+        const totalSlides = carousel.children.length;
+        currentIndex = (currentIndex + 1) % totalSlides;
+        updateCarousel(currentIndex);
+    };
+
+    const resetInterval = () => {
+        clearInterval(interval);
+        interval = setInterval(nextSlide, 3000); // Cambiar de diapositiva cada 3 segundos
+    };
+
+    window.addEventListener('resize', () => {
+        // Actualizar la posición del carrusel al redimensionar la ventana para mantenerlo centrado
+        updateCarousel(currentIndex);
+    });
+
+    interval = setInterval(nextSlide, 3000); // Cambio automático inicial de diapositiva cada 3 segundos
+});
