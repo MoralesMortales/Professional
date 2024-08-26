157385740580b2fd88e1fa1352c01e0220b87152
CarlosLup <carlos52.7624@gmail.com>
2024-08-26 11:50:07 -0400

    p1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base:'https://moralesmortales.github.io/professional',
   resolve: {
     alias: {
       '@': '/src',  
