0d614ae2d8088f4d2f4b040b4666096cd430ea08
MoralesMortales <carlos52.7624@gmail.com>
2026-02-13 02:11:45 -0400

    done

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'https://moralesmortales.github.io/professional',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
